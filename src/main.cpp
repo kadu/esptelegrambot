63bb1039c5749541b99c85a6b30044a21aae9cb7
kadu <kadu@kadu.com.br>
2021-08-02 15:59:37 -0300

    Adicionado e Testado a Fita de Led

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN       D1
+#define NUMPIXELS 20
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here,a to run once:
+  Serial.begin(115200);
+
+  pixels.begin();
+  pixels.clear();
+  pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+  pixels.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
